16757914150769e949203871f7bd54704967c300
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-11-12 21:22:42 +0400

    feat:  create equations

---
+++ src/components/Game/Rules/Equations/Equations.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import styles from './Equations.module.css';
+
+type EquationsProps = {
+  f1: number[],
+  f2: number[],
+}
+
+const getEquations = (f: number[]) => {
+  let result = ''
+  if (f[0] > 0) result += f[0] === 1? 'x' : `${f[0]}x`
+  else result += f[0] === -1? '-x' : `-${Math.abs(f[0])}x`;
+
+  if (f[1] > 0) result += f[1] === 1? ' + y' : ` + ${f[1]}y`
+  else result += f[1] === -1? '- y' : ` - ${Math.abs(f[1])}y`; 
+
+  if (f[2] > 0) result += ` + ${f[2]}`
+  else  result += ` - ${Math.abs(f[2])}`
+
+  return `${result} = 0`;
+}
+
+const Equations: FC<EquationsProps> = (props) => {
+  const { f1, f2 } = props;
+
+  return (
+    <div className={styles.contianer}>
+      <p>Line Equations</p> 
+      <div className={styles.equations}>{getEquations(f1)}</div>
+      <div className={styles.equations}>{getEquations(f2)}</div>
+    </div>
+  );
+}
+ 
+export default Equations;
